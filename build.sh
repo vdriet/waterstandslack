2da3ab6bc892f1e6f38ef7ffb0cf69f4fd50d89b
Peter <peter@kokosnoot>
2024-03-17 21:30:29 +0100

    Waterstand als module

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-pip install -r requirements.txt
+pip install --no-cache-dir --index-url https://test.pypi.org/simple/ --extra-index-url=https://pypi.org/simple/ -r requirements.txt
 pip list --outdated
 pylint *.py
 docker build --tag waterstandslack .
