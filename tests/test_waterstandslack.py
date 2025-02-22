2af74a4918f26713e9a7c6d53c9f07ae3d19dd35
Peter <peter@kokosnoot>
2025-02-22 22:43:59 +0100

    IT-214 nieuwe manier van integratie met slack

--- tests/test_waterstandslack.py
+++ tests/test_waterstandslack.py
@@ -1,188 +1,198 @@
 import os
-from unittest.mock import patch, Mock
+from unittest.mock import patch, Mock, MagicMock
 
 from freezegun import freeze_time
 
 import waterstandslack
+import responses
+import requests
 
-listresponse = Mock()
-listresponse.body = {'ok': 'true', 'channels': [{'id': 'DUMMYID', 'name': 'waterstand'}]}
+dummywebhook = 'https://hooks.slek.com/dummy'
+convlisturl = 'https://slack.com/api/conversations.list'
+convhisturl = 'https://slack.com/api/conversations.history'
+os.environ['KOKOSBOT_OAUTH_TOKEN'] = 'xoxo-dummy'
+os.environ['WATERSTAND_WEBHOOK_URL'] = dummywebhook
+listresponse={'ok': 'true', 'channels': [{'id': 'DUMMYID', 'name': 'waterstand'}]}
 
-histresponsenok = Mock()
-histresponsenok.body = {'ok': True, 'messages': [
-  {'subtype': 'bot_message', 'text': 'Gegevens RWS niet beschikbaar'}]}
-
-
-@patch('waterstand.haalwaterstand', return_value={'resultaat': 'NOK'})
-@patch('slacker.Chat.post_message', return_value=None)
-@patch('slacker.Conversations.list', return_value=listresponse)
-@patch('slacker.Conversations.history', return_value=histresponsenok)
-def test_main_nok(mock_converstations_history, mock_converstations_list, mock_post_message, mock_haalwaterstand,
-                  capsys):
-  os.environ['SLACK_ID_RASPBOT'] = 'DUMMY'
+@responses.activate
+def test_main_nok_rws(capsys):
+  responses.add(responses.POST, dummywebhook,
+                json={'status': 'ok'}, status=200)
+  responses.add(responses.GET, convlisturl,
+                json=listresponse, status=200)
+  responses.add(responses.POST, convhisturl,
+                json={'ok': True, 'messages': [{'subtype': 'bot_message', 'text': 'Gegevens RWS niet beschikbaar'}]}, status=200)
 
   waterstandslack.main()
 
   captured = capsys.readouterr()
   assert captured.out == 'Laatste bericht op slack: Gegevens RWS niet beschikbaar\n'
 
-  assert mock_haalwaterstand.called
-  assert mock_post_message.called
-  assert mock_converstations_list.called
-  assert mock_converstations_history.called
 
-
-histresponse = Mock()
-histresponse.body = {"ok": True, "messages": [
-  {"subtype": "bot_message", "text": "Stand 18-12 11:50 89.0, morgen 97.0", "ts": "1734519906.082659"}]}
-
-
-@patch('waterstand.haalwaterstand',
-       return_value={'resultaat': 'OK', 'tijd': '11:50', 'datum': '18-12', 'nu': 89.0, 'morgen': 97.0})
-@patch('slacker.Chat.post_message', return_value=None)
-@patch('slacker.Conversations.list', return_value=listresponse)
-@patch('slacker.Conversations.history', return_value=histresponse)
-@freeze_time("2024-12-18 12:05:00")
-def test_main_post_at_12(mock_converstations_history, mock_converstations_list, mock_post_message,
-                         mock_haalwaterstand, capsys):
-  os.environ['SLACK_ID_RASPBOT'] = 'DUMMY'
-
-  waterstandslack.main()
-
-  captured = capsys.readouterr()
-  assert captured.out == 'Laatste bericht op slack: Stand 18-12 11:50 89.0, morgen 97.0\n'
-
-  assert mock_haalwaterstand.called
-  assert mock_post_message.called
-  assert mock_converstations_list.called
-  assert mock_converstations_history.called
-
-
-@patch('waterstand.haalwaterstand',
-       return_value={'resultaat': 'OK', 'tijd': '11:50', 'datum': '18-12', 'nu': 89.0, 'morgen': 97.0})
-@patch('slacker.Chat.post_message', return_value=None)
-@patch('slacker.Conversations.list', return_value=listresponse)
-@patch('slacker.Conversations.history', return_value=histresponse)
-@freeze_time("2024-12-18 13:05:00")
-def test_main_no_post_at_13(mock_converstations_history, mock_converstations_list, mock_post_message,
-                            mock_haalwaterstand, capsys):
-  os.environ['SLACK_ID_RASPBOT'] = 'DUMMY'
-
-  waterstandslack.main()
-
-  captured = capsys.readouterr()
-  assert captured.out == 'Laatste bericht op slack: Stand 18-12 11:50 89.0, morgen 97.0\n'
-
-  assert mock_haalwaterstand.called
-  assert not mock_post_message.called
-  assert mock_converstations_list.called
-  assert mock_converstations_history.called
-
-
-@patch('waterstand.haalwaterstand',
-       return_value={'resultaat': 'OK', 'tijd': '11:50', 'datum': '18-12', 'nu': 107.0, 'morgen': 99.0})
-@patch('slacker.Chat.post_message', return_value=None)
-@patch('slacker.Conversations.list', return_value=listresponse)
-@patch('slacker.Conversations.history', return_value=histresponse)
-@freeze_time("2024-12-18 13:05:00")
-def test_main_post_nu_100_plus(mock_converstations_history, mock_converstations_list, mock_post_message,
-                               mock_haalwaterstand, capsys):
-  os.environ['SLACK_ID_RASPBOT'] = 'DUMMY'
-
-  waterstandslack.main()
-
-  captured = capsys.readouterr()
-  assert captured.out == 'Laatste bericht op slack: Stand 18-12 11:50 89.0, morgen 97.0\n'
-
-  assert mock_haalwaterstand.called
-  assert mock_post_message.called
-  assert mock_converstations_list.called
-  assert mock_converstations_history.called
-
-
-@patch('waterstand.haalwaterstand',
-       return_value={'resultaat': 'OK', 'tijd': '11:50', 'datum': '18-12', 'nu': 99.0, 'morgen': 107.0})
-@patch('slacker.Chat.post_message', return_value=None)
-@patch('slacker.Conversations.list', return_value=listresponse)
-@patch('slacker.Conversations.history', return_value=histresponse)
-@freeze_time("2024-12-18 13:05:00")
-def test_main_post_morgen_100_plus(mock_converstations_history, mock_converstations_list, mock_post_message,
-                                   mock_haalwaterstand, capsys):
-  os.environ['SLACK_ID_RASPBOT'] = 'DUMMY'
-
-  waterstandslack.main()
-
-  captured = capsys.readouterr()
-  assert captured.out == 'Laatste bericht op slack: Stand 18-12 11:50 89.0, morgen 97.0\n'
-
-  assert mock_haalwaterstand.called
-  assert mock_post_message.called
-  assert mock_converstations_list.called
-  assert mock_converstations_history.called
-
-
-@patch('waterstand.haalwaterstand',
-       return_value={'resultaat': 'OK', 'tijd': '11:50', 'datum': '18-12', 'nu': 33.0, 'morgen': 44.0})
-@patch('slacker.Chat.post_message', return_value=None)
-@patch('slacker.Conversations.list', return_value=listresponse)
-@patch('slacker.Conversations.history', return_value=histresponse)
-@freeze_time("2024-12-18 13:05:00")
-def test_main_post_stijging_10_plus(mock_converstations_history, mock_converstations_list, mock_post_message,
-                                    mock_haalwaterstand, capsys):
-  os.environ['SLACK_ID_RASPBOT'] = 'DUMMY'
-
-  waterstandslack.main()
-
-  captured = capsys.readouterr()
-  assert captured.out == 'Laatste bericht op slack: Stand 18-12 11:50 89.0, morgen 97.0\n'
-
-  assert mock_haalwaterstand.called
-  assert mock_post_message.called
-  assert mock_converstations_list.called
-  assert mock_converstations_history.called
-
-
-@patch('waterstand.haalwaterstand',
-       return_value={'resultaat': 'OK', 'tijd': '11:50', 'datum': '18-12', 'nu': 44.0, 'morgen': 33.0})
-@patch('slacker.Chat.post_message', return_value=None)
-@patch('slacker.Conversations.list', return_value=listresponse)
-@patch('slacker.Conversations.history', return_value=histresponse)
-@freeze_time("2024-12-18 13:05:00")
-def test_main_post_daling_10_plus(mock_converstations_history, mock_converstations_list, mock_post_message,
-                                  mock_haalwaterstand, capsys):
-  os.environ['SLACK_ID_RASPBOT'] = 'DUMMY'
-
-  waterstandslack.main()
-
-  captured = capsys.readouterr()
-  assert captured.out == 'Laatste bericht op slack: Stand 18-12 11:50 89.0, morgen 97.0\n'
-
-  assert mock_haalwaterstand.called
-  assert mock_post_message.called
-  assert mock_converstations_list.called
-  assert mock_converstations_history.called
-
-
-@patch('waterstand.haalwaterstand',
-       return_value={'resultaat': 'OK', 'tijd': '11:50', 'datum': '18-12', 'nu': 44.0, 'morgen': 33.0})
-@patch('slacker.Chat.post_message', return_value=None)
-@freeze_time("2024-12-18 13:05:00")
-def test_haalwaterstandenpost(mock_post_message, mock_haalwaterstand):
-  os.environ['SLACK_ID_RASPBOT'] = 'DUMMY'
-
-  waterstandslack.haalwaterstandenpost()
-
-  assert mock_haalwaterstand.called
-  assert mock_post_message.called
-
+@responses.activate
 @patch('waterstand.haalwaterstand',
        return_value={'resultaat': 'NOK', 'tekst': 'Foutmelding'})
-@patch('slacker.Chat.post_message', return_value=None)
+def test_main_nok_waterstand(mock_waterstand, capsys):
+  responses.add(responses.POST, dummywebhook,
+                json={'status': 'ok'}, status=200)
+  responses.add(responses.GET, convlisturl,
+                json=listresponse, status=200)
+  responses.add(responses.POST, convhisturl,
+                json={'ok': True, 'messages': [{'subtype': 'bot_message', 'text': 'Foutmelding'}]}, status=200)
+
+  waterstandslack.main()
+
+  captured = capsys.readouterr()
+  assert captured.out == 'Laatste bericht op slack: Foutmelding\n'
+
+
+@responses.activate
+@patch('waterstand.haalwaterstand',
+       return_value={'resultaat': 'OK', 'tijd': '11:50', 'datum': '18-12', 'nu': 89.0, 'morgen': 97.0})
+@freeze_time("2024-12-18 12:05:00")
+def test_main_post_at_12(mock_haalwaterstand, capsys):
+  responses.add(responses.POST, dummywebhook,
+                json={'status': 'ok'}, status=200)
+  responses.add(responses.GET, convlisturl,
+                json=listresponse, status=200)
+  responses.add(responses.POST, convhisturl,
+                json={"ok": True, "messages": [{"subtype": "bot_message", "text": "Stand 18-12 11:50 89.0, morgen 97.0", "ts": "1734519906.082659"}]}, status=200)
+
+  waterstandslack.main()
+
+  captured = capsys.readouterr()
+  assert captured.out == 'Laatste bericht op slack: Stand 18-12 11:50 89.0, morgen 97.0\n'
+  assert mock_haalwaterstand.called
+  assert len(responses.calls) == 3
+  assert responses.calls[0].request.url == dummywebhook
+
+
+@responses.activate
+@patch('waterstand.haalwaterstand',
+       return_value={'resultaat': 'OK', 'tijd': '11:50', 'datum': '18-12', 'nu': 89.0, 'morgen': 97.0})
 @freeze_time("2024-12-18 13:05:00")
-def test_haalwaterstandenpost_fout(mock_post_message, mock_haalwaterstand):
-  os.environ['SLACK_ID_RASPBOT'] = 'DUMMY'
+def test_main_no_post_at_13(mock_haalwaterstand, capsys):
+  responses.add(responses.POST, dummywebhook,
+                json={'status': 'ok'}, status=200)
+  responses.add(responses.GET, 'https://slack.com/api/conversations.list',
+                json=listresponse, status=200)
+  responses.add(responses.POST, 'https://slack.com/api/conversations.history',
+                json={"ok": True, "messages": [{"subtype": "bot_message", "text": "Stand 18-12 11:50 89.0, morgen 97.0", "ts": "1734519906.082659"}]}, status=200)
+
+  waterstandslack.main()
+
+  captured = capsys.readouterr()
+  assert captured.out == 'Laatste bericht op slack: Stand 18-12 11:50 89.0, morgen 97.0\n'
+
+  assert mock_haalwaterstand.called
+  assert len(responses.calls) == 2
+
+
+@responses.activate
+@patch('waterstand.haalwaterstand',
+       return_value={'resultaat': 'OK', 'tijd': '11:50', 'datum': '18-12', 'nu': 107.0, 'morgen': 99.0})
+@freeze_time("2024-12-18 13:05:00")
+def test_main_post_nu_100_plus(mock_haalwaterstand, capsys):
+  responses.add(responses.POST, dummywebhook,
+                json={'status': 'ok'}, status=200)
+  responses.add(responses.GET, 'https://slack.com/api/conversations.list',
+                json=listresponse, status=200)
+  responses.add(responses.POST, 'https://slack.com/api/conversations.history',
+                json={"ok": True, "messages": [{"subtype": "bot_message", "text": "Stand 18-12 11:50 107.0, morgen 99.0", "ts": "1734519906.082659"}]}, status=200)
+
+  waterstandslack.main()
+
+  captured = capsys.readouterr()
+  assert captured.out == 'Laatste bericht op slack: Stand 18-12 11:50 107.0, morgen 99.0\n'
+
+  assert mock_haalwaterstand.called
+  assert len(responses.calls) == 3
+
+
+@responses.activate
+@patch('waterstand.haalwaterstand',
+       return_value={'resultaat': 'OK', 'tijd': '11:50', 'datum': '18-12', 'nu': 99.0, 'morgen': 107.0})
+@freeze_time("2024-12-18 13:05:00")
+def test_main_post_morgen_100_plus(mock_haalwaterstand, capsys):
+  responses.add(responses.POST, dummywebhook,
+                json={'status': 'ok'}, status=200)
+  responses.add(responses.GET, 'https://slack.com/api/conversations.list',
+                json=listresponse, status=200)
+  responses.add(responses.POST, 'https://slack.com/api/conversations.history',
+                json={"ok": True, "messages": [{"subtype": "bot_message", "text": "Stand 18-12 11:50 99.0, morgen 107.0", "ts": "1734519906.082659"}]}, status=200)
+
+  waterstandslack.main()
+
+  captured = capsys.readouterr()
+  assert captured.out == 'Laatste bericht op slack: Stand 18-12 11:50 99.0, morgen 107.0\n'
+
+  assert mock_haalwaterstand.called
+  assert len(responses.calls) == 3
+
+
+@responses.activate
+@patch('waterstand.haalwaterstand',
+       return_value={'resultaat': 'OK', 'tijd': '11:50', 'datum': '18-12', 'nu': 33.0, 'morgen': 44.0})
+@freeze_time("2024-12-18 13:05:00")
+def test_main_post_stijging_10_plus(mock_haalwaterstand, capsys):
+  responses.add(responses.POST, dummywebhook,
+                json={'status': 'ok'}, status=200)
+  responses.add(responses.GET, 'https://slack.com/api/conversations.list',
+                json=listresponse, status=200)
+  responses.add(responses.POST, 'https://slack.com/api/conversations.history',
+                json={"ok": True, "messages": [{"subtype": "bot_message", "text": "Stand 18-12 11:50 33.0, morgen 44.0", "ts": "1734519906.082659"}]}, status=200)
+
+  waterstandslack.main()
+
+  captured = capsys.readouterr()
+  assert captured.out == 'Laatste bericht op slack: Stand 18-12 11:50 33.0, morgen 44.0\n'
+
+  assert mock_haalwaterstand.called
+  assert len(responses.calls) == 3
+
+
+@responses.activate
+@patch('waterstand.haalwaterstand',
+       return_value={'resultaat': 'OK', 'tijd': '11:50', 'datum': '18-12', 'nu': 44.0, 'morgen': 33.0})
+@freeze_time("2024-12-18 13:05:00")
+def test_main_post_daling_10_plus(mock_haalwaterstand, capsys):
+  responses.add(responses.POST, dummywebhook, status=200)
+  responses.add(responses.GET, 'https://slack.com/api/conversations.list',
+                json=listresponse, status=200)
+  responses.add(responses.POST, 'https://slack.com/api/conversations.history',
+                json={"ok": True, "messages": [{"subtype": "bot_message", "text": "Stand 18-12 11:50 44.0, morgen 33.0", "ts": "1734519906.082659"}]}, status=200)
+
+  waterstandslack.main()
+
+  captured = capsys.readouterr()
+  assert captured.out == 'Laatste bericht op slack: Stand 18-12 11:50 44.0, morgen 33.0\n'
+
+  assert mock_haalwaterstand.called
+  assert len(responses.calls) == 3
+
+
+@responses.activate
+@patch('waterstand.haalwaterstand',
+       return_value={'resultaat': 'OK', 'tijd': '11:50', 'datum': '18-12', 'nu': 44.0, 'morgen': 33.0})
+@freeze_time("2024-12-18 13:05:00")
+def test_haalwaterstandenpost(mock_haalwaterstand):
+  responses.add(responses.POST, dummywebhook,
+                json={'status': 'ok'}, status=200)
 
   waterstandslack.haalwaterstandenpost()
 
   assert mock_haalwaterstand.called
-  assert mock_post_message.called
+  assert len(responses.calls) == 1
+
+
+@responses.activate
+@patch('waterstand.haalwaterstand',
+       return_value={'resultaat': 'NOK', 'tekst': 'Foutmelding'})
+def test_haalwaterstandenpost_fout(mock_haalwaterstand):
+  responses.add(responses.POST, dummywebhook,
+                json={'status': 'ok'}, status=200)
+
+  waterstandslack.haalwaterstandenpost()
+
+  assert mock_haalwaterstand.called
+  assert len(responses.calls) == 1
--- waterstandslack.py
+++ waterstandslack.py
@@ -1,16 +1,17 @@
 """ Ophalen van de waterstand bij Zwolle en posten op slack """
 import os
 from datetime import datetime
-from slacker import Slacker
+import json
+import requests
 import waterstand
 
 
 def postberichtinwaterstand(bericht):
   """ zet een bericht in slack kanaal waterstand """
-  slackid = os.environ['SLACK_ID_RASPBOT']
-  slack = Slacker(slackid)
-
-  slack.chat.post_message('#waterstand', bericht)
+  webhookurl = os.environ['WATERSTAND_WEBHOOK_URL']
+  data = json.dumps({"text": bericht})
+  headers = {'Content-Type': 'application/json'}
+  requests.post(url=webhookurl, timeout=10, headers=headers, data=data)
 
 
 def postwaterstand(weergavetijd, hoogtenu, hoogtemorgen):
@@ -20,14 +21,25 @@ def postwaterstand(weergavetijd, hoogtenu, hoogtemorgen):
 
 def toonlaatstebericht():
   """ haal de laatste post van de waterstand op """
-  slackid = os.environ['SLACK_ID_RASPBOT']
-  slack = Slacker(slackid)
-  convlist = slack.conversations.list()
-  for channel in convlist.body['channels']:
+  conversationlisturl = 'https://slack.com/api/conversations.list'
+  token = os.environ['KOKOSBOT_OAUTH_TOKEN']
+  headers = {'Authorization': f'Bearer {token}',
+             'Content-Type': 'application/json'}
+  with requests.get(url=conversationlisturl,
+                    timeout=10,
+                    headers=headers) as response:
+    convlist = response.json()
+  for channel in convlist['channels']:
     if channel['name'] == 'waterstand':
       channelid = channel['id']
-      message = slack.conversations.history(channel=channelid, limit=1)
-      laatste = message.body['messages'][0]['text']
+      conversationhistoryurl = 'https://slack.com/api/conversations.history'
+      data=json.dumps({'channel': channelid})
+      with requests.post(url=conversationhistoryurl,
+                        timeout=10,
+                        headers=headers,
+                        data=data) as response:
+        message = response.json()
+      laatste = message['messages'][0]['text']
       print(f'Laatste bericht op slack: {laatste}')
 
 
@@ -35,7 +47,7 @@ def checkwaterstandenpost():
   """ haal de waterstand en zet die op slack als het aan de voorwaarden voldoet """
   gegevens = waterstand.haalwaterstand('Katerveer', 'KATV')
   if gegevens['resultaat'] == 'NOK':
-    postberichtinwaterstand(gegevens)
+    postberichtinwaterstand(gegevens['tekst'])
   else:
     weergavetijd = gegevens['tijd']
     hoogtenu = gegevens['nu']
